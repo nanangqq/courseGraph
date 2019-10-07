03ce35cfe2a267b9aecfdac5fc4fec6c4c9dd575
nanangqq <gingggg@gmail.com>
2019-10-07 16:50:19 +0900

    ...

--- map.py
+++ map.py
@@ -1,6 +1,6 @@
 import json
 
-with open('data/Postech.json', 'r', encoding='utf8') as f:
+with open('data/Microsoft.json', 'r', encoding='utf8') as f:
     a = json.load(f)
 
 # print(a)
@@ -12,5 +12,36 @@ print(tmpMap)
 
 a['map']=tmpMap
 
-with open('data/Postech_.json', 'w', encoding='utf8') as f:
+with open('data/Microsoft_.json', 'w', encoding='utf8') as f:
     json.dump(a,f)
+
+
+
+# from copy import copy
+
+# with open('data/SKKU.json', 'r', encoding='utf8') as f:
+#     a = json.load(f)
+
+# tmpCps = copy(a['cps'])
+# tmpMap = {}
+# for crs in a['crsList']:
+#     if crs['key']:
+#         tmpMap[crs['key']]=crs['props']['crs_id']
+#         # print(tmpCps.pop(int(crs['key'])))
+
+# for crs in a['crsList']:
+#     if not crs['key']:
+#         for i in range(len(tmpCps)):
+#             if str(i) not in tmpMap:
+#                 tmpMap[str(i)]=crs['props']['crs_id']
+#                 crs['key']=str(i)
+#                 crs['props']['keyFromParent']=i
+#                 crs['props']['cp']=tmpCps[i]
+#                 break
+# print(tmpMap)
+
+
+# a['map']=tmpMap
+
+# with open('data/SKKU_.json', 'w', encoding='utf8') as f:
+#     json.dump(a,f)
